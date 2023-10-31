cb40f7f5b59d33579b3062ba0c6c125f293601a2
Austin <arutherford@lakehillprep.org>
2023-10-31 09:58:43 -0500

    Merge pull request #731 from FIRST-Tech-Challenge/20230929-083754-release-candidate
    FtcRobotController v9.0.1

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vision/VisionForDetectingPropBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vision/VisionForDetectingPropBlue.java
@@ -17,35 +17,29 @@ public class VisionForDetectingPropBlue extends LinearOpMode {
 
     // Name of the Webcam to be set in the config
     private String webcamName = "Webcam 1";
-
     @Override
     public void runOpMode() throws InterruptedException {
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
         camera = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, webcamName), cameraMonitorViewId);
         org.firstinspires.ftc.teamcode.Vision.BlueColorProcessor blueColorProcessor = new BlueColorProcessor();
         camera.setPipeline(blueColorProcessor);
-
         camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
             @Override
             public void onOpened() {
                 camera.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT);
             }
-
             @Override
             public void onError(int errorCode) {
             }
         });
-
         while (!isStarted()) {
             telemetry.addData("Pos: ", blueColorProcessor.pos);
             telemetry.update();
         }
-
         waitForStart();
         while(opModeIsActive()){
             telemetry.addData("Pos: ", blueColorProcessor.pos);
             telemetry.update();
-
         }
     }
 }
